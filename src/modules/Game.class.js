0f9eb4b13b616193b5d4a63f883f369ae763d35e
Artur Shatskov <artur.shackov77@gmail.com>
2025-07-13 11:22:41 +0200

    Solution

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -6,10 +6,21 @@
  * Feel free to add more props and methods if needed.
  */
 class Game {
+  board = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+  score = 0;
+  status = 'idle';
+  rows = 4;
+  columns = 4;
+
   /**
    * Creates a new game instance.
    *
-   * @param {number[][]} initialState
+   * @param {number[][]} board
    * The initial state of the board.
    * @default
    * [[0, 0, 0, 0],
@@ -20,25 +31,80 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
+
+  constructor(board) {
     // eslint-disable-next-line no-console
-    console.log(initialState);
+    console.log(board);
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    let row;
+
+    for (let r = 0; r < this.rows; r++) {
+      row = this.board[r];
+      row = this.move(row);
+      this.board[r] = row;
+    }
+  }
+  moveRight() {
+    let row;
+
+    for (let r = 0; r < this.rows; r++) {
+      row = this.board[r];
+      row.reverse();
+      row = this.move(row);
+      row.reverse();
+      this.board[r] = row;
+    }
+  }
+  moveUp() {
+    for (let c = 0; c < this.columns; c++) {
+      const column = [];
+
+      for (let r = 0; r < this.rows; r++) {
+        column.push(this.board[r][c]);
+      }
+
+      const newColumn = this.move(column);
+
+      for (let r = 0; r < this.rows; r++) {
+        this.board[r][c] = newColumn[r];
+      }
+    }
+  }
+  moveDown() {
+    for (let c = 0; c < this.columns; c++) {
+      const column = [];
+
+      for (let r = 0; r < this.rows; r++) {
+        column.push(this.board[r][c]);
+      }
+
+      column.reverse();
+
+      const newColumn = this.move(column);
+
+      newColumn.reverse();
+
+      for (let r = 0; r < this.rows; r++) {
+        this.board[r][c] = newColumn[r];
+      }
+    }
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.board;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +116,123 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.status;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.generateRandom();
+    this.generateRandom();
+    this.status = 'playing';
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    this.score = 0;
+    this.start();
+  }
 
-  // Add your own methods here
+  move(inputRow) {
+    let row = inputRow;
+
+    row = this.filterZero(row);
+
+    for (let i = 0; i < row.length - 1; i++) {
+      if (row[i] === row[i + 1]) {
+        row[i] *= 2;
+        row[i + 1] = 0;
+        this.score += row[i];
+        i++;
+      }
+    }
+
+    row = this.filterZero(row);
+
+    while (row.length < this.columns) {
+      row.push(0);
+    }
+
+    return row;
+  }
+
+  filterZero(row) {
+    return row.filter((num) => num !== 0);
+  }
+
+  generateRandom() {
+    const options = [
+      { value: 2, probability: 0.9 },
+      { value: 4, probability: 0.1 },
+    ];
+
+    const randomValue = Math.random();
+    let cumulativeProbability = 0;
+    let value = 0;
+
+    // take a random value
+    for (const option of options) {
+      cumulativeProbability += option.probability;
+
+      if (randomValue <= cumulativeProbability) {
+        value = option.value;
+        break;
+      }
+    }
+
+    // replace random 0 with this value
+    while (true) {
+      const randomRow = Math.floor(Math.random() * this.board.length);
+      const randomCol = Math.floor(
+        Math.random() * this.board[randomRow].length,
+      );
+
+      if (this.board[randomRow][randomCol] === 0) {
+        this.board[randomRow][randomCol] = value;
+        break;
+      }
+    }
+  }
+
+  isAvailableMove() {
+    for (let r = 0; r < this.board.length; r++) {
+      for (let c = 0; c < this.board.length; c++) {
+        if (this.board[r][c] === 0) {
+          return true;
+        }
+
+        if (
+          c < this.board.length - 1 &&
+          this.board[r][c] === this.board[r][c + 1]
+        ) {
+          return true;
+        }
+
+        if (
+          r < this.board.length - 1 &&
+          this.board[r][c] === this.board[r + 1][c]
+        ) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  isWin() {
+    return this.board.some((row) => row.some((value) => value === 2048));
+  }
 }
 
 module.exports = Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,123 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+// #region getScore
+const gameScore = document.querySelector('.game-score');
+
+function actualizeScore() {
+  gameScore.textContent = game.getScore();
+}
+// #endregion
+
+// #region actualize board
+function actualizeBoard() {
+  const table = Array.from(document.querySelector('tbody').children);
+
+  for (let r = 0; r < table.length; r++) {
+    for (let c = 0; c < 4; c++) {
+      if (game.board[r][c] === 0) {
+        table[r].children[c].textContent = '';
+        table[r].children[c].className = 'field-cell';
+      } else {
+        table[r].children[c].textContent = game.board[r][c];
+
+        table[r].children[c].className =
+          `field-cell field-cell--${game.board[r][c]}`;
+      }
+    }
+  }
+}
+// #endregion
+
+// #region start game
+const startButton = document.querySelector('.button.start');
+
+startButton.addEventListener(
+  'click',
+  () => {
+    game.start();
+    actualizeBoard();
+
+    document.querySelector('.message-start').className =
+      'message message-start hidden';
+
+    startButton.className = 'button restart';
+    startButton.textContent = 'Restart';
+
+    startButton.addEventListener('click', () => {
+      game.restart();
+      actualizeBoard();
+      actualizeScore();
+    });
+
+    // #region game logic
+    const doc = document.querySelector('html');
+
+    doc.addEventListener('keydown', (e) => {
+      if (game.getStatus() === 'playing') {
+        if (
+          ['ArrowRight', 'ArrowLeft', 'ArrowDown', 'ArrowUp'].includes(e.key)
+        ) {
+          const boardBefore = game.board.map((row) => [...row]);
+
+          if (game.isAvailableMove()) {
+            if (e.key === 'ArrowRight') {
+              game.moveRight();
+            }
+
+            if (e.key === 'ArrowLeft') {
+              game.moveLeft();
+            }
+
+            if (e.key === 'ArrowDown') {
+              game.moveDown();
+            }
+
+            if (e.key === 'ArrowUp') {
+              game.moveUp();
+            }
+          }
+
+          const boardAfter = game.board;
+
+          let changed = false;
+
+          for (let r = 0; r < game.board.length; r++) {
+            for (let c = 0; c < game.board.length; c++) {
+              if (boardBefore[r][c] !== boardAfter[r][c]) {
+                changed = true;
+                break;
+              }
+            }
+          }
+
+          if (changed) {
+            game.generateRandom();
+            actualizeScore();
+            actualizeBoard();
+
+            if (game.isWin()) {
+              document.querySelector('.message-win').className =
+                'message message-win';
+              game.status = 'win';
+            }
+          } else {
+            if (game.isAvailableMove() === false) {
+              game.status = 'lose';
+
+              document.querySelector('.message-lose').className =
+                'message message-lose';
+            }
+          }
+        }
+      }
+    });
+
+    // #endregion
+  },
+  { once: true },
+);
+// #endregion
